c83894ebcee4545cb857f66ed1f0ff319764e38d
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-28 06:01:11 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
